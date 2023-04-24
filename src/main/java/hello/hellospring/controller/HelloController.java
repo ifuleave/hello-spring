9fa999db3104371227d7dd63591ed1529638259a
sujin <ifuleav_e@naver.com>
2023-04-25 01:02:16 +0900

    커밋

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,4 +23,39 @@ public class HelloController {
 
                 //localhost:8080/hello-mvc?name=spring!!!!!!이라고 하면  name에 spring!!!!!!이 들어가서 나옴
         }
+
+        //api방식
+        @GetMapping("hello-string")
+        @ResponseBody //http에서 통신 프로토콜에 body부분에 return 데이터를 직접 넣어 주겠다.
+        public String helloString(@RequestParam("name") String name){
+                return "hello"+name; // "hello spring"
+        }
+
+
+        @GetMapping("hello-api")
+        @ResponseBody
+        public Hello helloApi(@RequestParam("name") String name){
+                Hello hello = new Hello();
+                hello.setName(name);
+                return hello;
+        }
+        static class Hello{
+                private String name;
+                public String getName() {
+                        return name;
+                }
+                public void setName(String name) {
+                        this.name = name;
+                }
+        }
+        /*
+        @ResponseBody를 사용
+        http의 body에 문자 내용을 직접 반환
+        viewResolver대신에 http MessageConverter가 동작
+        기본 문자 처리 StringHttpMessageConverter
+        기본 객체 MappingJackson2HttpMessageConverter
+        byte 처리 등등 기타 여러 httpMessageConverter가 기본으로 등록되어 있음
+        * */
+
+
 }
