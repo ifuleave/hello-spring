803019b75147025dddbd43b3db4dc6a2e3f5b280
sujin <ifuleav_e@naver.com>
2023-04-25 23:37:05 +0900

    회원 도메인과 리포지토리 만들기 3-2

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,24 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id; //시스템에 데이터를 구분하기 위해 저장된  id
+    private  String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.lang.Nullable;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member); //스토어에 저장(map)에[ 저장됨
+        return member;
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //null일 경우 optional이라는 것으로 감싼다.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
