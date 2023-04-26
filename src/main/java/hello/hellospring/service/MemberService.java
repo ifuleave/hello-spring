55466f9e915c3578b3b712d8812885be1a039f0d
sujin <ifuleav_e@naver.com>
2023-04-26 23:56:02 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    * 회원가입
+    * */
+    
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);//중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) //옵셔널을 감싸면 옵셔널 안에 member가 있는거
+            .ifPresent(m -> {
+               throw new IllegalStateException("이미 존재하는 회원입니다.");
+           }); // ㅇㅣ부분 드래그 한다음 c+a+s+T 해서 7번 메서드 추출하면 -> 메서드로 따로 뺄 수 있음
+    }
+    
+    /*
+    *전체 회원조회
+    * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
