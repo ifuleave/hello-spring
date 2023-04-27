ce033b1cfad44fb4008242f6247aba952a3ef631
sujin <ifuleav_e@naver.com>
2023-04-28 00:18:24 +0900

    회원서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository; //외부에서 넣어주도록 바꾸기
+    }
 
     /*
     * 회원가입
